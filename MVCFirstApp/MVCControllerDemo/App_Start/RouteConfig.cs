adb766823470b4e70e90ea9b0ae963cb37fb9799
wadson <wadson.matos@consorciocpt.com.br>
2020-03-10 09:04:10 -0300

    Testing action url parameters

--- MVCFirstApp/MVCControllerDemo/App_Start/RouteConfig.cs
+++ MVCFirstApp/MVCControllerDemo/App_Start/RouteConfig.cs
@@ -13,6 +13,11 @@ namespace MVCControllerDemo
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Employee","Employee/{name}",new
+            {
+                controller = "Employee",action = "Search", name = UrlParameter.Optional
+            });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
