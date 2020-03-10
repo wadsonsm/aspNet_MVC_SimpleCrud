6842600e8bcf7245b162e415182f8a42cfc47185
wadson <wadson.matos@consorciocpt.com.br>
2020-03-10 13:20:50 -0300

    Estudando Actions

---
+++ MVCFirstApp/MVCControllerDemo/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControllerDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+       public string Index()
+       {
+            return @"<ul>
+                        <li> Ali Raza</li>
+                        <li> Mark Upston </li>
+                        <li> Allan Bommer </li>
+                        <li> Greg Jerry </li>
+                    <ul>";
+       }
+    }
+}
